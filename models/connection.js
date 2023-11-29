f6b4dfb36280d05b22fcd7fd5e26c21850dfac92
Arthurontime <local-git.squiggle185@passmail.net>
2023-11-29 18:16:41 +0100

    added console.log to try to fix MOngoDB connection in production

--- models/connection.js
+++ models/connection.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 const connectionString = process.env.MONGO_DB_CONNEXION
 
+console.log('trying to connect to Mongo DB...')
+
 // handle connection
 mongodb.connect( connectionString, {connectTimeoutMS:2000})
     .then(() => console.log('crmdiscord db connected'))
