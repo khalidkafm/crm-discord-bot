9c2aa6e6f0c950fa90e00c20fca12edd84b436e9
Arthurontime <local-git.squiggle185@passmail.net>
2023-11-30 14:26:24 +0100

    added search route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const Message = require('../models/messages')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/search', function(req, res, next) {
+  Message.find()
+  .then(data => res.json({data}))
 });
 
 module.exports = router;
