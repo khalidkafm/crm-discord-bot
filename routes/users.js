fc3f39e442ba20e0401d4dc6c7c303600c13ee9e
khalid <khalid91e@hotmail.fr>
2023-12-14 10:07:35 +0100

    modif route users

--- routes/users.js
+++ routes/users.js
@@ -37,14 +37,14 @@ router.get('/workspaces/:memberId', async (req, res, next) => {
     await client.guilds.fetch({ cache: false }); // mise à jour des guilds dans le cache
     if (client.guilds.cache.values()) {
       for (const guild of client.guilds.cache.values()) {
-        await guild.members.fetch({ user: "870287994173661254" }); // mise à jour des membres dans le cache
+        await guild.members.fetch({ user: req.params.memberId }); // mise à jour des membres dans le cache
         const mybot = guild.members.cache.get(botId);// robot ID
         if (mybot.permissions.toArray().includes('Administrator')) {
           if (guild.members.cache.values()) {
             for (const member of guild.members.cache.values()) {
               let memberInfo = {
                 guild_id: member.guild.id,
-                guild_name: member.guild.name,
+                guild_name: member.guild.name, 
                 user_id: member.id,
                 username: member.user.username,
                 roles_array: member.roles.cache.map((role) => role.id),
@@ -58,6 +58,55 @@ router.get('/workspaces/:memberId', async (req, res, next) => {
         }
       }
       res.send({ result: true, arraylength: newtableau.length, tableau: newtableau, });
+    }
+  }
+});
+
+router.get('/workspaces/:memberId/:guildId', async (req, res, next) => {
+  const client = await connectToDiscord(process.env.DISCORD_BOT_SECRET)
+//ma guilde : 1179468608410234941
+//mon ID : 1044963840632303659
+  if (client ) {//checker parce que parfois ça fait planter le lancement
+    const botId = client.user.id;
+    const theuser = [];
+    const theguild = [];
+    await client.guilds.fetch({ cache: false }); // mise à jour des guilds dans le cache
+    if (client.guilds.cache.values()) {
+      for (const guild of client.guilds.cache.values()) {
+        // console.log('je veux l ID de ma guild: ', guild.id)
+        if(guild.id ==req.params.guildId){
+          await guild.members.fetch(); // mise à jour des membres dans le cache
+        const mybot = guild.members.cache.get(botId);// robot ID
+        for(let permission of mybot.permissions.toArray()){
+          theguild.push(permission)
+        }
+
+        if (mybot.permissions.toArray().includes('Administrator')) {
+          if (guild.members.cache.values()) {
+            for (const member of guild.members.cache.values()) {
+              console.log(member.user.id)
+              if(member.user.id== req.params.memberId){
+                let memberInfo = {
+                  guild_id: member.guild.id,
+                  guild_name: member.guild.name,
+                  user_id: member.id,
+                  username: member.user.username,
+                  roles_array: member.roles.cache.map((role) => role.id),
+                  permissions: member.permissions.toArray(),
+                };
+                if (memberInfo.permissions.includes('ManageGuild')) {
+                  theuser.push(memberInfo);
+                };
+              }
+              
+            }
+          }
+        }
+      }
+        }
+
+        
+      res.send({ result: true, arraylength: theuser.length, permduuser: theuser[0].permissions, permdubot : theguild });
     }
   }
 });
